8f28079c65051e6afec6f4310befca814a7c339d
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2020-03-15 13:52:30 +0000

    setti aðferðir í FlightMananger

---
+++ src/controllers/FlightMananger.java
@@ -0,0 +1,52 @@
+package src.controllers;
+
+import src.datastructures.Flight;
+import src.datastructures.Airport;
+import java.util.Date;
+
+public class FlightMananger {
+  private Flight[] flights;
+
+  public FlightMananger() {
+  }
+
+  public Flight[] search() {
+    
+    return flights;
+  }
+
+  public Flight[] search(Date date) {
+    
+    return flights;
+  }
+
+  /*public Flight[] search(Airport from) {
+    
+    return flights;
+  }*/
+
+  public Flight[] search(Airport to) {
+    
+    return flights;
+  }
+
+  /*public Flight[] search(Date date, Airport from) {
+    
+    return flights;
+  }*/
+
+  public Flight[] search(Date date, Airport to) {
+    
+    return flights;
+  }
+
+  public Flight[] search(Airport from, Airport to) {
+    
+    return flights;
+  }
+
+  public Flight[] search(Date date, Airport from, Airport to) {
+    
+    return flights;
+  }
+}
