a380504dc65379984540c354f5952df99f4d0b2e
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2020-04-10 21:58:37 +0000

    Vantar að vinna rétt með hvaða sæti eru laus í gagnagrunninum, sjá BookingMananger

--- src/controllers/BookingMananger.java
+++ src/controllers/BookingMananger.java
@@ -9,8 +9,9 @@ import src.datastructures.*;
 
 public class BookingMananger {
   private static Booking[] bookings;
+  private static String availableSeats;
 
-  public static void ConnectToPerson(String sql, String[] gildi, Boolean insert) throws ClassNotFoundException {
+  public static void ConnectToBooking(String sql, String[] gildi, String type) throws ClassNotFoundException {
     Class.forName("org.sqlite.JDBC");
     Connection connection = null;
     try {
@@ -22,11 +23,16 @@ public class BookingMananger {
         ps.setString(i, gildi[i - 1]);
       }
       ps.setQueryTimeout(30);
-      if (insert) {
+      if (type.equals("update")) {
         ps.executeUpdate();
-      } else {
+      } else if(type.equals("getSeats")) {
         ResultSet resultSet = ps.executeQuery();
-        // Vantar
+        availableSeats = resultSet.getString("available_seats");
+      } else if(type.equals("getBookings")) {
+        ResultSet resultSet = ps.executeQuery();
+        while(resultSet.next()) {
+          // Búa til booking út frá resultSet og setja í bookings
+        }
       }
     } catch (SQLException e) {
       System.err.println(e.getMessage());
@@ -40,13 +46,25 @@ public class BookingMananger {
     }
   }
 
-  public static void createBooking(Booking b, Person p) {
-    // Hér þarf að uppfæra airplain þannig að sætið sem var verið
-    // að bóka.
-
+  public static void createBooking(Booking b, Person p) throws ClassNotFoundException {
+    // Uppfærum available_seat breytunna í töflunni Airplain
+    String sql = "SELECT available_seats FROM Airplain;";
+    ConnectToBooking(sql, new String[0], "getSeats");
+    // Uppfæra availableSeats breytuna
+    // availableSeats = ...   ATHUGA AÐ ÞESSI LÍNA Á EKKI AÐ VERA KOMMENT
+    sql = "INSERT INTO Airplain(available_seats) values(?);";
+    String[] gildi = {availableSeats};
+    ConnectToBooking(sql, gildi, "update");
     // Bæta við bókun í Booking töfluna.
-
+    sql = "INSERT INTO Booking(row, seat, person, flight, " +
+          "booking_time) values(?,?,?,?,?);";
+    String[] nyGildi = {b.getRow()+"", b.getSeat()+"", p.getSsn(),
+        b.getFlight().getFlightNumber(), b.getBookingTime().toString()};
+    ConnectToBooking(sql, nyGildi, "update");
     // Uppfæra bookings í person töflunni fyrir person p.
+    // Ég held að það sé óþarfi að hafa dálkinn bookings í person
+    // töflunni því við getum sameinað töflurnar booking og person
+    // með SQL skipunum
   }
 
   public static void delete(Booking b) {
@@ -55,7 +73,7 @@ public class BookingMananger {
 
   public static Booking[] getBookings(Person p) {
     // Ef við viljum bjóða uppá að prenta út allar bókanir
-    // fyrir einhverja manneskju
+    // fyrir einhverja manneskju p
     return bookings;
   }
 }
